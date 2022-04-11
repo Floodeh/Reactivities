833a8de3efc1bef51def2c57a82afbf9ca45263d
Adam Flood <adamflood888@hotmail.com>
2022-04-11 13:33:29 +0100

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,15 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
         }
 
 
@@ -56,6 +65,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
